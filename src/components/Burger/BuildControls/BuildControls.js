6c074b97cef9e6c083995b9c0366af08499d5668
Krik Tsakma <Krik Tsakmatzian>
2018-07-27 01:25:07 +0300

    adding order button, modal & backdrop

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,6 +13,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {
             controls.map((ctrl) => (
                 <BuildControl
@@ -22,8 +23,11 @@ const buildControls = (props) => (
                      removed={()=>props.ingredientsRemoved(ctrl.type)}
                      disabled={props.disabled[ctrl.type]}/>
             ))
-
         }
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW</button>
     </div>
 )
 
