48d589f3db92275ca7f5e522269851b0d0ff5f9f
unknown <Krik Tsakmatzian>
2018-07-26 21:30:50 +0300

    first home commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MyAux from '../../hoc/MyAux';
+import classes from './Layout.css';
+
+const layout = (props) => (
+    <MyAux>
+        <div>Toolbar, Sidebar, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </MyAux>
+);
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import MyAux from '../../hoc/MyAux';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
+        return (
+            <MyAux>
+                <Burger ingredients={this.state.ingredients} />
+                <div>
+                    Build Controls
+                </div>
+            </MyAux>          
+        );
+    }
+
+}
+
+export default BurgerBuilder;
